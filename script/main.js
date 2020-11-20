c02623dacf829f98885bcdac48df4afe0f69ccd1
Medvedev Nikolay <MedvedevKlaus@yandex.ru>
2020-11-20 14:22:14 +0300

    дз по уроку №2

--- script/main.js
+++ script/main.js
@@ -5,5 +5,22 @@ let money,
     mission,
     period;
 
-alert('любой текст');
-console.log('с любым текстом');
+money = 60000;
+income = 'фриланс, инвестиции';
+addExpenses = 'Интернет, такси, комунналка, еда, машина';
+deposit = true;
+mission = 1000000;
+period = 9;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев', '\nЦель заработать', mission, 'Рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log('Дневной бюджет =', budgetDay);
