c0e721da915aa1a18613f230f6d230527d6d044c
Nik Medvedev <MedvedevKlaus@yandex.ru>
2020-12-01 23:49:54 +0300

    fix(js/main.js): fixing the appearence of elements

--- js/main.js
+++ js/main.js
@@ -7,14 +7,23 @@ const btnStart = document.getElementById('start'),
   btnExpenses = document.getElementsByTagName('button')[1],
   checkDeposit = document.querySelector('#deposit-check'),
   itemIncome = document.querySelectorAll('.additional_income-item'),
-  elemValue = document.querySelectorAll('[class$="-value"]'),
+  budgetMonthValue = document.getElementsByClassName('budget_month-value')[0],
+  budgetDayValue = document.getElementsByClassName('budget_day-value')[0],
+  expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0],
+  additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0],
+  additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0],
+  incomePeriodValue = document.getElementsByClassName('income_period-value')[0],
+  targetMonthValue = document.getElementsByClassName('target_month-value')[0],
   inputMoney = document.querySelector('.salary-amount'),
-  inputIncomeTitle = document.querySelector('.income-title'),
-  inputIncomeAmount = document.querySelector('.income-amount'),
-  inputIncomeTitle = document.querySelector('.income-title'),
-  inputExpenses = document.querySelector('[class^="additional"]'),
+  incomeTitle = document.querySelector('input.income-title'),
+  incomeAmount = document.querySelector('.income-amount'),
+  expensesTitle = document.querySelector('input.expenses-title'),
+  expensesAmount = document.querySelector('.expenses-amount'),
+  additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+  depositPercent = document.querySelector('.deposit-percent'),
+  depositAmount = document.querySelector('.deposit-amount'),
   inputTarget = document.querySelector('.target-amount'),
-  inputPeriod = document.querySelector('.period-select');
+  selectPeriod = document.querySelector('.period-select');
 
 
   // вывод в консоль этих элементов
@@ -24,11 +33,20 @@ console.log('btnIncome: ', btnIncome);
 console.log('btnExpenses: ', btnExpenses);
 console.log('checkDeposit: ', checkDeposit);
 console.log('itemIncome: ', itemIncome);
-console.log('elemValue: ', elemValue);
+console.log('budgetMonthValue: ', budgetMonthValue);
+console.log('budgetDayValue: ', budgetDayValue);
+console.log('expensesMonthValue: ', expensesMonthValue);
+console.log('additionalExpensesValue: ', additionalExpensesValue);
+console.log('additionalIncomeValue: ', additionalIncomeValue);
+console.log('incomePeriodValue: ', incomePeriodValue);
+console.log('targetMonthValue: ', targetMonthValue);
 console.log('inputMoney: ', inputMoney);
-console.log('inputIncomeTitle: ', inputIncomeTitle);
-console.log('inputIncomeAmount: ', inputIncomeAmount);
-console.log('inputIncomeTitle: ', inputIncomeTitle);
-console.log('inputExpenses: ', inputExpenses);
+console.log('inputIncomeTitle: ', incomeTitle);
+console.log('inputIncomeAmount: ', incomeAmount);
+console.log('expensesTitle: ', expensesTitle);
+console.log('expensesAmount: ', expensesAmount);
 console.log('inputTarget: ', inputTarget);
-console.log('inputPeriod: ', inputPeriod);
+console.log('depositPercent: ', depositPercent);
+console.log('depositAmount: ', depositAmount);
+console.log('inputPeriod: ', selectPeriod);
+console.log('additionalExpensesItem: ', additionalExpensesItem);
